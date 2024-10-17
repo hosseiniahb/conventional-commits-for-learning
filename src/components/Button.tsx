9e7731ed8fae34272bc5d1e086e988014a7b143c
aliasgharhosseinibagheri_ <aliasgharhosseinibagheri@gmail.com>
2024-10-17 14:40:23 +0330

    fix: add the missing onclick event for the button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,17 @@
-import { ReactNode } from "react";
+import { MouseEventHandler, ReactNode } from "react";
 
-const Button = ({ children }: { children: ReactNode }) => {
+const Button = ({
+  children,
+  onClick,
+}: {
+  children: ReactNode;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}) => {
   return (
-    <button className="py-2 px-4 rounded-xl font-semibold text-xs bg-primary">
+    <button
+      onClick={onClick}
+      className="py-2 px-4 rounded-xl font-semibold text-xs bg-primary"
+    >
       {children}
     </button>
   );
